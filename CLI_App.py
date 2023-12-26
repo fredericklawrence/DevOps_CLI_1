d462c2bd1d22e45e9df1266e4f340a9c43e98e59
fredericklawrence <eerriiccllaaww@gmail.com>
2023-12-25 21:46:43 -0600

    Simple proof of concept

--- CLI_App.py
+++ CLI_App.py
@@ -12,7 +12,7 @@ def run_command(command):
         )
         return result.stdout, result.stderr
     except Exception as e:
-        return None, str(e) #comment 2
+        return None, str(e)
 
 def create_user(username):
     print(f"Creating user: {username}")
